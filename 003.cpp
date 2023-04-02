0ec736a74e3071ac5552f2477cff9888a9fe3c6b
君忘逝梦繁华 <127771114+Jacaranda-mimosifolia@users.noreply.github.com>
2023-04-02 15:38:54 +0800

    Add files via upload

---
+++ 003.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if(a>c)
+		{
+			printf("%d",a);
+		}
+		else if(a<c)
+		{
+			printf("%d",c);
+		}
+	}
+	else if(a<b)
+	{
+		if(b>c)
+		{
+			printf("%d",b);
+		}
+		else if(b<c)
+		{
+			printf("%d",c);
+		}
+	}
+	else
+	{
+		printf("ÇëÖØÐÂÊäÈë"); 
+	}
+	return 0;
+ } 
